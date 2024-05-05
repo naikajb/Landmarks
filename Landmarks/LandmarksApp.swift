00d0cf88479fc037fd4f180eb42579549f2a59c9
Naika JB <jbaptiste.naika@gmail.com>
2024-05-05 12:45:30 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Naïka Jean-Baptiste on 2024-05-05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
